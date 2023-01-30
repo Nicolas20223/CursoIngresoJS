88ff6b16a69bba3b55308ce9b53ec183581c60a5
unknown <Nicolas20223>
2023-01-30 12:53:40 -0300

    cambio de ejercicios

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,16 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	// alert("hola");
+	// var edad;
+	let nombreCompleto;// Notacion Lower camel case
+
+	// nombre = "messi"; // Asignacion de string.
+	nombre = prompt ("ingrese un nombre")
+
+	// mostrar por alert
+	alert (nombre);
+
+
 }
 
